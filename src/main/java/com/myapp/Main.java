939ffabc9cb98c1b2ebac406466525e1bb17069b
aaronestradabonilla777 <lacomarcaz@hotmail.com>
2024-01-04 22:59:20 -0600

    Refactor code to use Java 8 stream for string reversal

--- src/main/java/com/myapp/Main.java
+++ src/main/java/com/myapp/Main.java
@@ -1,12 +1,25 @@
 package com.myapp;
-
+import java.util.stream.Collectors;
 // Invertir cadena de texto
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+//         System.out.println("Ingresa una cadena de texto:");
+//         String input = scanner.nextLine();
+//         String reversed = new StringBuilder(input).reverse().toString();
+//         System.out.println("Cadena invertida: " + reversed);
+//     }
+// }
+
+//metodo 2
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Ingresa una cadena de texto:");
-        String input = scanner.nextLine();
-        String reversed = new StringBuilder(input).reverse().toString();
-        System.out.println("Cadena invertida: " + reversed);
+        String str = "Hola Mundo";
+        String reversed = new StringBuilder(str.chars()
+            .mapToObj(c -> (char)c)
+            .collect(Collectors.toList()))
+            .reverse()
+            .toString();
+        System.out.println(reversed);
     }
 }
