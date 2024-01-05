d6f071778f65a9edd839f32ec39402ad83dc82d1
aaronestradabonilla777 <lacomarcaz@hotmail.com>
2024-01-04 23:38:57 -0600

    Refactor Main.java and add hello-world-java project

--- src/main/java/com/myapp/Main.java
+++ src/main/java/com/myapp/Main.java
@@ -1,29 +1,43 @@
 package com.myapp;
+
 import java.util.stream.Collectors;
+// File: Main.java
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter a string:");
+    String input = scanner.nextLine();
+    String reversed = new StringBuilder(input).reverse().toString();
+    System.out.println("Reversed string: " + reversed);
+  }
+}
 // Invertir cadena de texto
 // public class Main {
-//     public static void main(String[] args) {
-//         Scanner scanner = new Scanner(System.in);
-//         System.out.println("Ingresa una cadena de texto:");
-//         String input = scanner.nextLine();
-//         String reversed = new StringBuilder(input).reverse().toString();
-//         System.out.println("Cadena invertida: " + reversed);
-//     }
+// public static void main(String[] args) {
+// Scanner scanner = new Scanner(System.in);
+// System.out.println("Ingresa una cadena de texto:");
+// String input = scanner.nextLine();
+// String reversed = new StringBuilder(input).reverse().toString();
+// System.out.println("Cadena invertida: " + reversed);
+// }
 // }
 
-//metodo 2
-public class Main {
-    public static void main(String[] args) {
-        String str = "Hola Mundo";
-        String reversed = new StringBuilder(str.chars()
-            .mapToObj(c -> (char)c)
-            .collect(Collectors.toList()))
-            .reverse()
-            .toString();
-        System.out.println(reversed);
-    }
-}
-//3metodo
+// metodo 2
+// public class Main {
+// public static void main(String[] args) {
+// String str = "Hola Mundo";
+// String reversed = new StringBuilder(str.chars()
+// .mapToObj(c -> (char)c)
+// .collect(Collectors.toList()))
+// .reverse()
+// .toString();
+// System.out.println(reversed);
+// }
+// }
+// 3metodo
 // string text = "Hola Mundo";
 // string textResult = "";
 
@@ -32,3 +46,5 @@ public class Main {
 // textResult = new string(chars);
 
 // console.WriteLine(textResult);
+// }
+// }
---
+++ target/classes/com/myapp/hello-world-java/README.md
@@ -0,0 +1,22 @@
+# Hello World in Java
+
+This is a simple project that runs a Hello World program in Java.
+
+## Requirements
+
+For building and running the application you need:
+
+- JDK 1.8
+- Maven 3
+
+## Running the application locally
+
+There are several ways to run a Spring Boot application on your local machine. One way is to execute the `main` method in the `com.example.HelloWorld` class from your IDE.
+
+Alternatively you can use the [Spring Boot Maven plugin](https://docs.spring.io/spring-boot/docs/current/reference/html/build-tool-plugins-maven-plugin.html) like so:
+
+```shell
+mvn spring-boot:run
+```
+```
+Please replace the content inside the shell script with the appropriate command to run your Java application if it's not a Spring Boot application.
